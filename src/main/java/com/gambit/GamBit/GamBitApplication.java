a4d64944c05ad24200473a2c016fe7f69d4f3bbf
1KitCat1 <denischool119@gmail.com>
2022-10-31 00:10:50 +0200

    Initial commit

---
+++ src/main/java/com/gambit/GamBit/GamBitApplication.java
@@ -0,0 +1,13 @@
+package com.gambit.GamBit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamBitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamBitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gambit/GamBit/GamBitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gambit.GamBit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamBitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
