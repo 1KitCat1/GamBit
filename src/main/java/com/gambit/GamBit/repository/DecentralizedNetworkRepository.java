df1e93f3c517970193929cd01d7135e630b17699
1KitCat1 <denischool119@gmail.com>
2022-11-02 13:18:15 +0200

    Added decentralized network repository

---
+++ src/main/java/com/gambit/GamBit/repository/DecentralizedNetworkRepository.java
@@ -0,0 +1,8 @@
+package com.gambit.GamBit.repository;
+
+import com.gambit.GamBit.model.DecentralizedNetwork;
+import org.springframework.data.repository.CrudRepository;
+
+public interface DecentralizedNetworkRepository extends CrudRepository<DecentralizedNetwork, Long> {
+
+}
--- src/main/java/com/gambit/GamBit/repository/SmartContractRepository.java
+++ src/main/java/com/gambit/GamBit/repository/SmartContractRepository.java
@@ -4,5 +4,5 @@ import com.gambit.GamBit.model.SmartContract;
 import org.springframework.data.repository.CrudRepository;
 
 public interface SmartContractRepository extends CrudRepository<SmartContract, Long> {
-    
+
 }
