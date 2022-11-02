6d133d42c16c1fc21d7852fa3a0fb3180b7187ff
1KitCat1 <denischool119@gmail.com>
2022-11-02 02:29:37 +0200

    Created Smart Contract entity

---
+++ src/main/java/com/gambit/GamBit/model/SmartContract.java
@@ -0,0 +1,65 @@
+package com.gambit.GamBit.model;
+
+public class SmartContract {
+    private Long id;
+    private Long decentralizedNetworkID;
+    private String address;
+    private String ABI;
+
+    public SmartContract() { }
+
+    public SmartContract(Long id, Long decentralizedNetworkID, String address, String ABI) {
+        this.id = id;
+        this.decentralizedNetworkID = decentralizedNetworkID;
+        this.address = address;
+        this.ABI = ABI;
+    }
+
+    public SmartContract(Long decentralizedNetworkID, String address, String ABI) {
+        this.decentralizedNetworkID = decentralizedNetworkID;
+        this.address = address;
+        this.ABI = ABI;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getDecentralizedNetworkID() {
+        return decentralizedNetworkID;
+    }
+
+    public void setDecentralizedNetworkID(Long decentralizedNetworkID) {
+        this.decentralizedNetworkID = decentralizedNetworkID;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getABI() {
+        return ABI;
+    }
+
+    public void setABI(String ABI) {
+        this.ABI = ABI;
+    }
+
+    @Override
+    public String toString() {
+        return "SmartContract{" +
+                "id=" + id +
+                ", decentralizedNetworkID=" + decentralizedNetworkID +
+                ", address='" + address + '\'' +
+                ", ABI='" + ABI + '\'' +
+                '}';
+    }
+}
