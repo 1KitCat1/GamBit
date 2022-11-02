559686a1c124eadf3c7f6c6d427c493c312b3bb0
1KitCat1 <denischool119@gmail.com>
2022-11-02 13:15:54 +0200

    Added smart contract repository

---
+++ src/main/java/com/gambit/GamBit/repository/SmartContractRepository.java
@@ -0,0 +1,8 @@
+package com.gambit.GamBit.repository;
+
+import com.gambit.GamBit.model.SmartContract;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SmartContractRepository extends CrudRepository<SmartContract, Long> {
+    
+}
