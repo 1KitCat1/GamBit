357bae2f488990368cbd81991ea901a1d46531b1
1KitCat1 <denischool119@gmail.com>
2022-11-22 22:37:29 +0200

    Added Game entity

---
+++ src/main/java/com/gambit/GamBit/model/Game.java
@@ -0,0 +1,23 @@
+package com.gambit.GamBit.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity(name = "game") @Data
+public class Game {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(name = "tokens_amount")
+    LocalDateTime dateTime;
+
+    @Column(name = "game_score")
+    Long gameScore;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name="contract_id", referencedColumnName = "id")
+    SmartContract smartContract;
+}
