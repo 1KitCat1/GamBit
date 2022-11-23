5211c1d55fe3482ce75ce42c143084124b7ead67
1KitCat1 <denischool119@gmail.com>
2022-11-23 23:00:07 +0200

    Implemented genereting parameters in game

--- src/main/java/com/gambit/GamBit/service/impl/GameServiceImpl.java
+++ src/main/java/com/gambit/GamBit/service/impl/GameServiceImpl.java
@@ -8,14 +8,28 @@ import com.gambit.GamBit.service.GameService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
+import java.util.Random;
 
 @Service
 @RequiredArgsConstructor
 public class GameServiceImpl implements GameService {
     private final GameRepository gameRepository;
+    private final Random random = new Random(LocalDateTime.now().getNano());
     @Override
     public void addGame(Game game) {
+        if(game.getDateTime() == null) { game.setDateTime(LocalDateTime.now()); }
+        if(game.getGameScore() == null) {
+            game.setGameScore(Math.abs(random.nextLong()) % 1000);
+        }
+        if(game.getRandomSalt() == null) {
+            game.setRandomSalt(
+                    ((Long)Math.abs(random.nextLong())).toString() +
+                    ((Long)Math.abs(random.nextLong())).toString()
+            );
+        }
+
         gameRepository.save(game);
     }
 
